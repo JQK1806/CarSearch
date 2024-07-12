d2585bd1124930f5e4c65c370baa4ceada364411
JQK1806 <king.jos@northeastern.edu>
2024-07-11 22:45:44 -0400

    Get car models and engine information
    Models of the cars can now be selected. Engine information is provided for the given car.

--- main/app.py
+++ main/app.py
@@ -3,14 +3,15 @@ import streamlit as st
 from config import api_key, partner_key
 
 
-# Get all avaiable car makes for the year
-def get_car_makes(year):
-    url = f'https://api.carmd.com/v3.0/make?year={year}'
-    headers = {
+headers = {
         'content-type': 'application/json',
         'authorization': api_key,
         'partner-token': partner_key
     }
+
+# Get all avaiable car makes for the given year
+def get_car_makes(year):
+    url = f'https://api.carmd.com/v3.0/make?year={year}'
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
@@ -22,8 +23,36 @@ def get_car_makes(year):
     else:
         st.error(f"Error: {response.status_code} - {response.json().get('message', 'Unknown error')}")
         return []
-
-
+    
+# Get all avaiable car models for the given make and year
+def get_car_models(year, make):
+    url = f'https://api.carmd.com/v3.0/model?year={year}&make={make}'
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data:
+            return data['data']
+        else:
+            st.error("No data found for car models.")
+            return []
+    else:
+        st.error(f"Error: {response.status_code} - {response.json().get('message', 'Unknown error')}")
+        return []
+    
+# Get the avaiable engine information about the given year, make, and model of the car
+def get_car_engine(year, make, model):
+    url = f'http://api.carmd.com/v3.0/engine?year={year}&make={make}&model={model}'
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data:
+            return data['data']
+        else:
+            st.error("No data found for engine data.")
+            return None
+    else:
+        st.error(f"Error: {response.status_code} - {response.json().get('message', 'Unknown error')}")
+        return None
 
 
 
@@ -32,6 +61,7 @@ st.title("Car Search App")
 
 year = st.text_input("Enter the year:")
 car_makes = []
+car_models = []
 
 if year:
     try:
@@ -46,5 +76,16 @@ else:
     make = None
 
 if make:
-    st.write(f"You have selected {make} for the year {year}")
+    car_models = get_car_models(year, make)
+    if car_models:
+        model = st.selectbox("Select the model:", car_models)
+    else:
+        model = None
 
+if year and make and model:
+    engine_data = get_car_engine(year, make, model)
+    if engine_data:
+        st.write(f"Engine data for {year}, {make}, {model}.")
+        st.json(engine_data)
+    else:
+        st.write("No engine data found.")
